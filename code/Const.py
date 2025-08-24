b0f5fae374c65b061b12cc2d0e38fd2018146572
João Nahid <joaonahiddev@gmail.com>
2025-08-24 00:48:37 -0300

    :sparkles: Feature: Create const for entities speed

--- code/Const.py
+++ code/Const.py
@@ -5,3 +5,15 @@ WIN_HEIGHT = 324
 COLOR_TEXT_ORANGE = (255,128,0)
 COLOR_TEXT_ORANGE_700 = (255,128,0, 0.7)
 COLOR_TEXT_WHITE = (255,255,255)
+
+# Speed
+ENTITY_SPEED = {
+    # Level 1
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+}
