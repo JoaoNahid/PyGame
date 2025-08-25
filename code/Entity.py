85494db1110a2997fde710e9e83fe73c1872369d
João Nahid <joaonahiddev@gmail.com>
2025-08-25 19:31:42 -0300

    :hammer: Refactor: Convert Entity image to alpha

--- code/Entity.py
+++ code/Entity.py
@@ -7,7 +7,7 @@ class Entity(ABC):
 
     def __init__(self, name: str, position: tuple, speed: int):
         self.name = name
-        self.surf = pygame.image.load('./assets/images/' + name + '.png')
+        self.surf = pygame.image.load('./assets/images/' + name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = speed
 
